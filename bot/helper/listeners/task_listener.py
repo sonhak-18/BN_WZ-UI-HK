5e3a67e61d9095c267a4d5a2bcf47f78bddb9f64
sonhak-18 <fawaid1808@gmail.com>
2025-07-01 17:54:04 +0700

    Force

--- bot/helper/listeners/task_listener.py
+++ bot/helper/listeners/task_listener.py
@@ -484,7 +484,6 @@ class TaskListener(TaskConfig):
                         if mime_type == "Folder":
                            share_url += '/' 
                            buttons.url_button("⚡ Index Link", share_url)
-                    else:
                         if mime_type.startswith(("image", "video", "audio")):
                             share_urls = f"{INDEX_URL}/{url_path}?a=view"
                             buttons.url_button("🌐 View Link", share_urls)
