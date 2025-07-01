ee992d1fccfb4cee160c774c6487876d2024b0db
sonhak-18 <fawaid1808@gmail.com>
2025-07-01 16:54:31 +0700

    Ui Adjustment: search - Initial adjustment

--- bot/helper/mirror_leech_utils/gdrive_utils/search.py
+++ bot/helper/mirror_leech_utils/gdrive_utils/search.py
@@ -134,16 +134,16 @@ class GoogleDriveSearch(GoogleDriveHelper):
                 msg += f"<h4>Search Result For {file_name}</h4>"
                 Title = True
             if drive_name:
-                msg += f"╾────────────╼<br><b>{drive_name}</b><br>╾────────────╼<br>"
+                msg += f"<br><b>❏ {drive_name}</b><br><br>"
             for file in response.get("files", []):
                 mime_type = file.get("mimeType")
                 if mime_type == self.G_DRIVE_DIR_MIME_TYPE:
                     furl = self.G_DRIVE_DIR_BASE_DOWNLOAD_URL.format(file.get("id"))
-                    msg += f"📁 <code>{file.get('name')}<br>(folder)</code><br>"
-                    msg += f"<b><a href={furl}>Drive Link</a></b>"
+                    msg += f"📁 <b>{file.get('name')}<br>(folder)</b><br>"
+                    msg += f"<b><a href={furl}>☁️ Drive Link</a></b>"
                     if index_url:
                         url = f"{index_url}findpath?id={file.get('id')}"
-                        msg += f' <b>| <a href="{url}">Index Link</a></b>'
+                        msg += f' <b>| <a href="{url}">⚡️ Index Link</a></b>'
                 elif mime_type == "application/vnd.google-apps.shortcut":
                     furl = self.G_DRIVE_DIR_BASE_DOWNLOAD_URL.format(file.get("id"))
                     msg += (
@@ -152,14 +152,14 @@ class GoogleDriveSearch(GoogleDriveHelper):
                     )
                 else:
                     furl = self.G_DRIVE_BASE_DOWNLOAD_URL.format(file.get("id"))
-                    msg += f"📄 <code>{file.get('name')}<br>({get_readable_file_size(int(file.get('size', 0)))})</code><br>"
-                    msg += f"<b><a href={furl}>Drive Link</a></b>"
+                    msg += f"📄 <b>{file.get('name')}<br>({get_readable_file_size(int(file.get('size', 0)))})</b><br>"
+                    msg += f"<b><a href={furl}>☁️ Drive Link</a></b>"
                     if index_url:
                         url = f"{index_url}findpath?id={file.get('id')}"
-                        msg += f' <b>| <a href="{url}">Index Link</a></b>'
+                        msg += f' <b>| <a href="{url}">⚡️ Index Link</a></b>'
                         if mime_type.startswith(("image", "video", "audio")):
                             urlv = f"{index_url}findpath?id={file.get('id')}&view=true"
-                            msg += f' <b>| <a href="{urlv}">View Link</a></b>'
+                            msg += f' <b>| <a href="{urlv}">👀 View Link</a></b>'
                 msg += "<br><br>"
                 contents_no += 1
                 if len(msg.encode("utf-8")) > 39000:
