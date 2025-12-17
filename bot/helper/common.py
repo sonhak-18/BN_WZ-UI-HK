e5df8b021f026ce32d6b82ab8855120e13b78f65
Riajul <93116400+rjriajul@users.noreply.github.com>
2025-12-17 08:52:50 +0600

    feat: include FORUM chat type

--- bot/helper/common.py
+++ bot/helper/common.py
@@ -436,12 +436,7 @@ class TaskConfig:
                         self.hybrid_leech = False
                     else:
                         uploader_id = TgClient.user.me.id
-                        if chat.type.name not in [
-                            "SUPERGROUP",
-                            "CHANNEL",
-                            "GROUP",
-                            "FORUM",
-                        ]:
+                        if chat.type.name not in ["SUPERGROUP", "CHANNEL", "GROUP", "FORUM"]:
                             self.user_transmission = False
                             self.hybrid_leech = False
                         else:
@@ -465,12 +460,7 @@ class TaskConfig:
                             raise ValueError("Chat not found!")
                     else:
                         uploader_id = self.client.me.id
-                        if chat.type.name in [
-                            "SUPERGROUP",
-                            "CHANNEL",
-                            "GROUP",
-                            "FORUM",
-                        ]:
+                        if chat.type.name in ["SUPERGROUP", "CHANNEL", "GROUP", "FORUM"]:
                             member = await chat.get_member(uploader_id)
                             if (
                                 not member.privileges.can_manage_chat
