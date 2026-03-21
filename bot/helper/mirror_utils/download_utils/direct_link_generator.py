a803a6709dbec03db094d040c6f351281cc98f0b
Son Hak <support.bawera.nitijana@darpana.web.id>
2026-03-21 07:49:01 +0700

    Revert "Update PD direct download link"
    This reverts commit c2be5bd53a56cf1d48be4901526026efd200538d.

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -599,7 +599,7 @@ def direct_link_generator(link):
         return hxfile(link)
     elif "1drv.ms" in domain:
         return onedrive(link)
-    elif any(x in domain for x in ["pixeldrain.com", "pixeldra.in"]):
+    elif "pixeldrain.com" in domain:
         return pixeldrain(link)
     elif "antfiles.com" in domain:
         return antfiles(link)
@@ -983,13 +983,25 @@ def onedrive(link):
 
 
 def pixeldrain(url):
-    try:
-        url = url.rstrip("/")
-        code = url.split("/")[-1].split("?", 1)[0]
-        response = get("https://pd.cybar.xyz/", allow_redirects=True)
-        return response.url + code
-    except Exception as e:
-        raise DirectDownloadLinkException("ERROR: Direct link not found")
+    url = url.strip("/ ")
+    file_id = url.split("/")[-1]
+    if url.split("/")[-2] == "l":
+        info_link = f"https://pixeldrain.com/api/list/{file_id}"
+        dl_link = f"https://pixeldrain.com/api/list/{file_id}/zip?download"
+    else:
+        info_link = f"https://pixeldrain.com/api/file/{file_id}/info"
+        dl_link = f"https://pixeldrain.com/api/file/{file_id}?download"
+    with create_scraper() as session:
+        try:
+            resp = session.get(info_link).json()
+        except Exception as e:
+            raise DirectDownloadLinkException(f"ERROR: {e.__class__.__name__}") from e
+    if resp["success"]:
+        return dl_link
+    else:
+        raise DirectDownloadLinkException(
+            f"ERROR: Cant't download due {resp['message']}."
+        )
 
 
 def antfiles(url):
